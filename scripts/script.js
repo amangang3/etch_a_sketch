7cab676c59f3d6e99cf0025d59f1b19dff65a1d5
amangang3 <amangang3@gmail.com>
2020-08-31 22:14:42 +0400

    got a 4x4 grid using js

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,15 @@
 gridSize = 16;
 
-function createGrid() {
-    let container = document.getElementById('gridContainer');
-    container.innerHTML = '';
-    var lengthOfBox = Math.floor(window.innerWidth / gridSize);
-    for (var i = 0; (lengthOfBox * lengthOfBox); i++) {
-        var box = document.createElement('div');
-        box.className = 'box';
-        container.appendChild(box);
-    }
-}
+const container = document.getElementById("container");
+
+function makeRows(rows, cols) {
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
+    for (c = 0; c < (rows * cols); c++) {
+        let cell = document.createElement("div");
+        cell.innerText = 1;
+        container.appendChild(cell).className = "grid-item";
+    };
+};
+
+makeRows(4, 4); 
